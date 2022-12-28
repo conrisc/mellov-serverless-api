75c4213879cd55a245bf68febd4b9127eb6fb706
Konrad C <conrisc@gmail.com>
2022-12-28 21:19:27 +0100

    Keep db client reference in global scope
    It allows to reuse existing db client connection between Lambda invocations

--- src/services/database.service.mjs
+++ src/services/database.service.mjs
@@ -3,19 +3,21 @@ import { resolveSecret } from './secrets.service.mjs';
 
 const dbCluster = process.env.DB_CLUSTER;
 const dbCredentialsSecretName = process.env.DB_CREDENTIALS_SECRET_NAME;
+let dbClient;
 
-// TODO: Resolve secrets once and connect to db once
 export async function getDB() {
-    const dbCredentials = await resolveSecret(dbCredentialsSecretName);
-    const {
-        username: dbUsername,
-        password: dbPassword,
-    } = dbCredentials;
-    const uri = `mongodb+srv://${dbUsername}:${dbPassword}@${dbCluster}?retryWrites=true&w=majority`;
-    const client = new MongoClient(uri, { useUnifiedTopology: true });
+    if (!dbClient) {
+        const dbCredentials = await resolveSecret(dbCredentialsSecretName);
+        const { username: dbUsername, password: dbPassword } = dbCredentials;
+        const uri = `mongodb+srv://${dbUsername}:${dbPassword}@${dbCluster}?retryWrites=true&w=majority`;
+        dbClient = new MongoClient(uri, { useUnifiedTopology: true });
 
-    await client.connect();
-    console.log('Connected successfully to the database');
-    const database = client.db();
+        await dbClient.connect();
+        console.log('Connected successfully to the database');
+    } else {
+        console.log('Reusing existing db connection');
+    }
+
+    const database = dbClient.db();
     return database;
 }
