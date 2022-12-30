5c261017f19244bee274d8b2612459b6d2e269a9
Konrad C <conrisc@gmail.com>
2022-12-30 18:57:05 +0100

    Add User-Agent header to allowed headers

--- src/utils/cors-headers.util.mjs
+++ src/utils/cors-headers.util.mjs
@@ -1,6 +1,6 @@
 export function corsHeaders() {
     return {
-        'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key',
+        'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,User-Agent',
         'Access-Control-Allow-Methods': '*',
         'Access-Control-Allow-Origin': '*',
     }
