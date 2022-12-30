d29c3777961443a6ef6a1a86fe44d4109a4a2f90
Konrad C <conrisc@gmail.com>
2022-12-30 12:38:54 +0100

    Define and implement Add Song endpoint

---
+++ src/handlers/add-song.mjs
@@ -0,0 +1,49 @@
+import { getDB } from "../services/database.service.mjs";
+import { corsHeaders } from "../utils/cors-headers.util.mjs";
+
+export const addSongHandler = async (event) => {
+    if (event.httpMethod !== 'POST') {
+        throw new Error(`addSongHandler only accept POST method, you tried: ${event.httpMethod}`);
+    }
+
+    console.info('received:', event);
+    const collectionName = 'songs';
+
+    let response;
+    let songItem;
+    try {
+        songItem = JSON.parse(event.body);
+    } catch (error) {
+        console.error(`Failed to parse body: ${error}`);
+
+        response = {
+            statusCode: 400,
+            headers: corsHeaders(),
+            body: 'Failed to parse body - invalid Song object',
+        };
+    }
+
+    if (songItem) {
+        try {
+            const db = await getDB();
+            const collection = db.collection(collectionName);
+
+            const insertResult = await collection.insertOne(songItem);
+            response = {
+                statusCode: 201,
+                headers: corsHeaders(),
+                body: insertResult.insertedId.toHexString()
+            };
+        } catch (error) {
+            console.log(`Error while inserting song: ${error}`);
+            response = {
+                statusCode: 400,
+                headers: corsHeaders(),
+                body: 'Failed to add the song',
+            };
+        }
+    }
+
+    console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
+    return response;
+}
